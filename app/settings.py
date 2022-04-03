ba508f2b1ba98d61c233491f6c29cf22e5e4f63f
yjpeng82 <yjpeng82@gmail.com>
2022-04-03 13:39:32 -0700

    account UI updated

--- app/settings.py
+++ app/settings.py
@@ -127,7 +127,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "shopper", "static"),
+    os.path.join(BASE_DIR, "account", "static"),
+]
 
 # To easily get csrftoken for fetch.
 # Include {% csrf_token %} in html,
