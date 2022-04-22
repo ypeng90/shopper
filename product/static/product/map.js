c016eda99540d3c3c8f955affe8c0f01fbf328a7
yjpeng82 <yjpeng82@gmail.com>
2022-04-21 17:23:26 -0700

    refactored

--- product/static/product/map.js
+++
@@ -1,5 +0,0 @@
-const copy = "© <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors";
-const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-const osm = L.tileLayer(url, { attribution: copy });
-const map = L.map("map", { layers: [osm] });
-map.fitWorld();
